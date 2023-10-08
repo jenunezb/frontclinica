7461dba9c44a90430e446095bc3d4d4d461b33dd
jenunezb <jenunezb@uqvirtual.edu.co>
2023-10-08 16:11:29 -0500

    front clinica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pagina/inicio/inicio.component';
+import { LoginComponent } from './pagina/login/login.component';
+import { RegistroMedicoComponent } from './pagina/registro-medico/registro-medico.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    LoginComponent,
+    RegistroMedicoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modelo/medico-dto.ts
@@ -0,0 +1,14 @@
+export class MedicoDTO {
+    nombre:string = "";
+    cedula:number = 0;
+    ciudad:number = 0;
+    especialidad:number = 0;
+    telefono:string = "";
+    correo: string = "";
+    password: string = "";
+    confirmaPassword: string = "";
+    horaInicioJornada: string = "";
+    horaFinJornada: string = "";
+    urlFoto: string = "";
+}
+
