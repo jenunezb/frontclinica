69ea85364f6e7b6b90eafaa7a6e02bb24de71491
jenunezb <jenunezb@uqvirtual.edu.co>
2023-10-25 16:03:49 -0500

    Configuración de Angular con estilos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './pagina/inicio/inicio.component';
 import { LoginComponent } from './pagina/login/login.component';
-import { RegistroMedicoComponent } from './pagina/registro-medico/registro-medico.component';
-import { HorarioComponent } from './pagina/horario/horario.component';
 const routes: Routes = [
 { path: "", component: InicioComponent },
 { path: "login", component: LoginComponent },
-{ path: "registro", component: RegistroMedicoComponent },
-{path: "horario", component: HorarioComponent},
 { path: "**", pathMatch: "full", redirectTo: "" }
 ];
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './pagina/inicio/inicio.component';
 import { LoginComponent } from './pagina/login/login.component';
-import { RegistroMedicoComponent } from './pagina/registro-medico/registro-medico.component';
-import { HorarioComponent } from './pagina/horario/horario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     LoginComponent,
-    RegistroMedicoComponent,
-    HorarioComponent
   ],
   imports: [
     BrowserModule,
