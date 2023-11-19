6a2ca64cba3a2ff0944840b00b93ad08e57b4be5
jenunezb <jenunezb@uqvirtual.edu.co>
2023-11-19 04:16:37 -0500

    eliminar médico

---
+++ src/app/modelo/item-medico.dto.ts
@@ -0,0 +1,10 @@
+export class ItemMedicoDTO {
+   cedula: string ="";
+   nombre: string="";
+   urlFoto: string="";
+   especialidad: string="";
+   horaInicio: string="";
+   horaFin: string="";
+   codigo: number=0;
+
+}
