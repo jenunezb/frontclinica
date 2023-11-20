f46dbd55e46f54f2e2dc5c45e89a0dac0e6b369b
jenunezb <jenunezb@uqvirtual.edu.co>
2023-11-20 03:58:27 -0500

    puliendo las citas para los pacientes tambien

--- src/app/pagina/listar-citas/listar-citas.component.ts
+++ src/app/pagina/listar-citas/listar-citas.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ItemCitaAdminDTO } from 'src/app/modelo/item-cita-admin-dto';
 import { AdministradorService } from 'src/app/servicios/administrador.service';
+import { UsuarioService } from 'src/app/servicios/paciente.service';
+import { TokenService } from 'src/app/servicios/token.service';
 
 @Component({
   selector: 'app-listar-citas',
@@ -10,24 +12,30 @@ import { AdministradorService } from 'src/app/servicios/administrador.service';
 })
 export class ListarCitasComponent {
   public listaCitas: ItemCitaAdminDTO[] = [];
+  esAdmin: boolean = false;
 
-  constructor(private administardorService: AdministradorService, private router : Router){}
+  constructor(private administardorService: AdministradorService, private router : Router,private tokenService: TokenService,
+    private pacienteService: UsuarioService){}
 
   ngOnInit(): void {
     this.listarCitas();
+    if(this.tokenService.getRole()[0]=="a"){
+      this.esAdmin=true;
+    }
   }
 
   public listarCitas(): void{
-this.administardorService.listarCitas().subscribe(
-  (response: any) => {
-    console.log('Respuesta del servicio:', response.respuesta);
-    this.listaCitas = response.respuesta;
-},
-error => {
-  console.error('Error al obtener la lista de citas', error);
-}
-);
-
+    if(this.tokenService.getRole()[0]=="a"){
+      this.administardorService.listarCitas().subscribe(
+        (response: any) => {
+          console.log('Respuesta del servicio:', response.respuesta);
+          this.listaCitas = response.respuesta;
+      },
+      error => {
+        console.error('Error al obtener la lista de citas', error);
+      }
+      );
+    }
   }
 
   public editarCita(){
